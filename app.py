0f54743868d1840a772affd31ce2e10cdae909a3
SwthSawTheme <sawtheme1@gmail.com>
2024-11-21 19:59:35 -0300

    funcao register adicionada

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect,url_for
 
 app = Flask(__name__)
 
-users = {}
-items = []
+# Simulação de base de dados (apenas para teste)
+users = {} # {'username': 'password'}
+items = [] # [{'name': 'item', 'quantity': 2, 'value': 10.5}]
 
 # Rota para login
 @app.route('/login', methods=['GET','POST'])
@@ -15,3 +16,16 @@ def login():
             return redirect(url_for('item'))
         return "Login inválido", 403
     return render_template('login.html')
+
+# Rota para cadastro de itens
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            return "Usuário ja existe", 409
+        users[username] = password
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
